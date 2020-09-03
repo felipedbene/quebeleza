e9135842a3a6a8bb06f3915e6bf5915cf3da9b07
felipe jimenez <felipe@localhost.localdomain>
2020-09-02 23:08:43 -0500

    Better looking

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './imgs/quebeleza2.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
-          Proximamente, Que Beleza !
+          Proximamente....
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
     </div>
   );
